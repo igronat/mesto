27948eb577ce1c2af16c393d0020820bea1f870e
Natalya Radina <igronat@yandex.ru>
2021-11-16 19:40:03 +0300

    Проектная работа 4 исправлены замечания

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,27 @@
-const editButton = document.querySelector(".profile__edit-button_link");
-const popup = document.querySelector(".popup");
-const popupClose = popup.querySelector(".popup__close");
+const editButton = document.querySelector('.profile__edit-button_link');
+const popup = document.querySelector('.popup');
+const popupClose = popup.querySelector('.popup__close');
+let profileTitle = document.querySelector('.profile__title');
+let profileText = document.querySelector('.profile__text');
+let formElement = popup.querySelector('.popup__content')
+let nameInput = popup.querySelector('.popup__text_type_name');
+let jobInput = popup.querySelector('.popup__text_type_job');
 
 function open() {
-    popup.classList.add("popup__opened");
+    popup.classList.add('popup_opened'); //функцция открытия окна popup
 }
 
 function close() {
-    popup.classList.remove("popup__opened");
+    popup.classList.remove('popup_opened'); //функцция закрытия окна popup
 }
 
-editButton.addEventListener("click", open);
-popupClose.addEventListener("click", close);
-
-let formElement = popup.querySelector(".popup__content")
-let nameInput = popup.querySelector(".popup__text_name");
-let jobInput = popup.querySelector(".popup__text_job");
-
-
-function formSubmitHandler(evt) {
+function formSubmitHandler(evt) { //функция внесения изменений в профиль
     evt.preventDefault();
-    let profileTitle = document.querySelector(".profile__title");
-    let profileText = document.querySelector(".profile__text");
     profileTitle.textContent = nameInput.value;
     profileText.textContent = jobInput.value;
     close();
 }
 
+editButton.addEventListener('click', open);
+popupClose.addEventListener('click', close);
 formElement.addEventListener('submit', formSubmitHandler);
