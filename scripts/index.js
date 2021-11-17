b9ea01abeff2c152674aac8dcda6b326550d62d3
Natalya Radina <igronat@yandex.ru>
2021-11-17 14:33:58 +0300

    Проектная работа 4 исправлены замечания popup

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,16 @@
-const editButton = document.querySelector('.profile__edit-button_link');
+const editButton = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
 const popupClose = popup.querySelector('.popup__close');
 let profileTitle = document.querySelector('.profile__title');
 let profileText = document.querySelector('.profile__text');
-let formElement = popup.querySelector('.popup__content');
+let formElement = popup.querySelector('.popup__form');
 let nameInput = popup.querySelector('.popup__text_type_name');
 let jobInput = popup.querySelector('.popup__text_type_job');
 
 function open() {
     popup.classList.add('popup_opened'); //функцция открытия окна popup
+    nameInput.value = profileTitle.textContent; // выводим в инпут данные из профиля
+    jobInput.value = profileText.textContent;
 }
 
 function close() {
