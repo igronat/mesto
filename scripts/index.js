754293f4713add24ce47ce8bf16892d94e1c8123
Natalya Radina <igronat@yandex.ru>
2021-12-03 10:31:38 +0300

    add popupMesto

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,45 @@
 const editButton = document.querySelector('.profile__edit-button');
-const popup = document.querySelector('.popup');
+const addButton = document.querySelector('.profile__add-button');
+const popup = document.getElementById('profile');
+console.log(popup);
+const popupMesto = document.getElementById('mesto');
+console.log(popupMesto);
 const popupClose = popup.querySelector('.popup__close');
+console.log(popupClose);
+const popupCloseMesto = popupMesto.querySelector('.popup__close');
+console.log(popupCloseMesto);
+const buttonSave = popup.querySelector('button-save');
+const buttonCreate = popupMesto.querySelector('button-create');
 let profileTitle = document.querySelector('.profile__title');
 let profileText = document.querySelector('.profile__text');
 let formElement = popup.querySelector('.popup__form');
 let nameInput = popup.querySelector('.popup__text_type_name');
 let jobInput = popup.querySelector('.popup__text_type_job');
 
-function open() {
-    popup.classList.add('popup_opened'); //функцция открытия окна popup
-    nameInput.value = profileTitle.textContent; // выводим в инпут данные из профиля
-    jobInput.value = profileText.textContent;
+function open(evt) {
+    evt.classList.add('popup_opened'); //функция открытия окна popup 
+
 }
 
-function close() {
-    popup.classList.remove('popup_opened'); //функцция закрытия окна popup
+function close(evt) {
+    evt.classList.remove('popup_opened'); //функция закрытия окна popup 
+
 }
 
 function formSubmitHandler(evt) { //функция внесения изменений в профиль
     evt.preventDefault();
     profileTitle.textContent = nameInput.value;
     profileText.textContent = jobInput.value;
-    close();
+    close(popup);
 }
 
-editButton.addEventListener('click', open);
-popupClose.addEventListener('click', close);
+editButton.addEventListener('click', () => {
+    nameInput.value = profileTitle.textContent; // выводим в инпут данные из профиля
+    jobInput.value = profileText.textContent;
+    open(popup);
+});
+
+addButton.addEventListener('click', () => open(popupMesto));
+popupClose.addEventListener('click', () => close(popup));
+popupCloseMesto.addEventListener('click', () => close(popupMesto));
 formElement.addEventListener('submit', formSubmitHandler);
