f3734dae4414ea36b316dbabc6ceefc8cec852f6
Natalya Radina <igronat@yandex.ru>
2021-12-04 17:48:39 +0300

    add Big img

--- scripts/index.js
+++ scripts/index.js
@@ -8,6 +8,7 @@ const buttonCreate = document.getElementById('button-create');
 let profileTitle = document.querySelector('.profile__title');
 let profileText = document.querySelector('.profile__text');
 let formElement = popup.querySelector('.popup__form');
+let formMesto = document.querySelector('.popup__mesto');
 let nameInput = popup.querySelector('.popup__text_type_name');
 let jobInput = popup.querySelector('.popup__text_type_job');
 
@@ -41,7 +42,9 @@ const elements = document.querySelector('.elements');
 const templateEl = document.querySelector('.template');
 let mesto = document.querySelector('.popup__text_type_mesto');
 let link = document.querySelector('.popup__text_type_link');
-
+const popupImg = document.getElementById('img');
+const popupCloseImg = popupImg.querySelector('.popup__close');
+console.log(popupImg);
 
 
 function open(evt) {
@@ -61,7 +64,8 @@ function formSubmitHandler(evt) { //функция внесения измене
     close(popup);
 }
 
-function cardSubmitHandler() { //функция добавления карточки
+function cardSubmitHandler(evt) { //функция добавления карточки
+    evt.preventDefault();
     const inputMesto = mesto.value;
     const inputImage = link.value;
     const cardItem = getItem({
@@ -95,12 +99,21 @@ function getItem(item) {
         const target = event.target;
         const card = document.querySelector('.element');
         card.remove();
-
     });
 
     const buttonHeart = newItem.querySelector('.element__heart'); //ставим лайк
     buttonHeart.addEventListener('click', () =>
         buttonHeart.classList.add('element__heart_active'));
+
+    imageEl.addEventListener('click', () => { //открываем попап для увеличения картинки
+        open(popupImg);
+        const imageElBigSize = popupImg.querySelector('.element__image_bigsize');
+        const titleElBigSize = popupImg.querySelector('.element__title_bigsize');
+        imageElBigSize.src = imageEl.src;
+        titleElBigSize.textContent = titleEl.textContent;
+        console.log(imageElBigSize);
+
+    });
 
     return newItem;
 
@@ -116,5 +129,6 @@ editButton.addEventListener('click', () => {
 addButton.addEventListener('click', () => open(popupMesto));
 popupClose.addEventListener('click', () => close(popup));
 popupCloseMesto.addEventListener('click', () => close(popupMesto));
+popupCloseImg.addEventListener('click', () => close(popupImg));
 formElement.addEventListener('submit', formSubmitHandler);
-buttonCreate.addEventListener('click', cardSubmitHandler);
+formMesto.addEventListener('submit', cardSubmitHandler);
